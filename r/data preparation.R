9d959f166f18013f25e11630c2b9162eb635f155
sren-dfci <sren@ds.dfci.harvard.edu>
2020-11-18 17:41:37 -0500

    switch from 8-17 data to 10-23

--- r/data preparation.R
+++ r/data preparation.R
@@ -1,19 +1,17 @@
-if (! "data.table" %in% installed.packages()[, 'Package']) {
-  install.packages('data.table')
-}
-
 library(data.table)
+library(tidyverse)
 
-data_path <- "../data/2020-8-17/"
-redcap_data <- fread(paste0(data_path, "RegistryOfPregnancyA_DATA_2020-08-17_0951.csv"))
+data_path <- "C:/Users/sren/Dropbox (Partners HealthCare)/BOC shared/Chemo during pregnancy (Sella)/data/2020-10-23"
+redcap_data <- fread(file.path(
+  data_path, "RegistryOfPregnancyA_DATA_2020-10-23_1015.csv"
+))
 names(redcap_data)[1] <- "record_id"
-redcap_data <- redcap_data %>% filter(! record_id %in% c(26, 56, 128)) # exclude record ID 26, 56, 128
-
+# exclude record ID 26, 56, 128
+redcap_data <- redcap_data[!record_id %in% c(26, 56, 128), ]
 # gestational age at checmotherapy initiation is not numeric
-redcap_data$chemoga[redcap_data$chemoga == "n/a"] <- NA
-redcap_data$chemoga[redcap_data$chemoga == "unknown"] <- NA
-redcap_data$chemoga[redcap_data$chemoga == "18 weeks"] <- "18"
-redcap_data$chemoga <- as.numeric(redcap_data$chemoga)
+redcap_data[chemoga %in% c("n/a", "unknown"), chemoga := NA]
+redcap_data[chemoga == "18 weeks", chemoga := "18"]
+redcap_data[, chemoga := as.numeric(chemoga)]
 
 # table 1 changes
 # cancer type
@@ -30,7 +28,11 @@ redcap_data[anychemo == 1 & neoadjchemo == 0, chemo_timing := "adjuvant"]
 # GCSF
 redcap_data[gfpreg == 0, gcsf := "no"]
 redcap_data[gfpreg == 1 & gftype == "daily GCSF", gcsf := "daily_gcsf"]
-redcap_data[gfpreg == 1 & gftype %in% c("depot GCSF", "Neulasta", "Pegfilgrastim"), gcsf := "depot_gcsf"]
+redcap_data[
+  gfpreg == 1 &
+    gftype %in% c("depot GCSF", "Neulasta", "Pegfilgrastim"),
+  gcsf := "depot_gcsf"
+]
 # Chemotherapy
 redcap_data[chemopg == 0, chemotherapy := "no chemo"]
 redcap_data[acpreg == 1 & taxolpreg == 0, chemotherapy := "AC_only"]
@@ -49,9 +51,15 @@ redcap_data[ptd == 0, preterm_birth_outcome := ">=37"]
 redcap_data[ptd == 1, preterm_birth_outcome := "<37"]
 redcap_data[sptb == 1, preterm_birth_reason := "Spontaneous"]
 redcap_data[indptb == 1, preterm_birth_reason := "Medical"]
-redcap_data[sptb == 1 & indptb == 1, preterm_birth_reason := "Spontaneous & Medical"]
+redcap_data[
+  sptb == 1 & indptb == 1,
+  preterm_birth_reason := "Spontaneous & Medical"
+]
 redcap_data[iatptb == 1, preterm_birth_reason := "Iatrogenic"]
-redcap_data[sptb == 0 & indptb == 0 & iatptb == 0, preterm_birth_reason := "Unknown"]
+redcap_data[
+  sptb == 0 & indptb == 0 & iatptb == 0,
+  preterm_birth_reason := "Unknown"
+]
 redcap_data[ptd == 0, preterm_birth_reason := "Not applicable"]
 
 # Table 3 changes
@@ -63,4 +71,3 @@ redcap_data[, obstetrical := as.numeric(rowSums(.SD, na.rm = TRUE) > 0), .SDcols
 redcap_data[, gestational := as.numeric(rowSums(.SD, na.rm = TRUE) > 0), .SDcols = c("sga", "apgar5v7", "anomalies")]
 
 write.csv(redcap_data, file = paste0(data_path, "cleaned_data.csv"), row.names = FALSE)
-
