6a27d941b3c2c1d5a4bb4978af6c86757e574271
sren-dfci <sren@ds.dfci.harvard.edu>
2020-12-12 20:46:35 -0500

    change import data location

--- r/functions.R
+++ r/functions.R
@@ -201,3 +201,84 @@ StdDiff <- function(.grp, .var, type = "numeric") {
   # print(paste0(type, " standardized difference"))
   return(.result)
 }
+
+extractATE <- function(.psw) {
+  .wt_estimator <- .psw[
+    c(
+      "est.risk.wt", "std.risk.wt", "est.rr.wt", "std.rr.wt",
+      "est.or.wt", "std.or.wt"
+    )
+  ] # list
+  .wt_estimator <- data.frame(
+    matrix(
+      unlist(.wt_estimator),
+      ncol = length(.wt_estimator)
+    )
+  )
+  colnames(.wt_estimator) <- c(
+    "est.risk.wt", "std.risk.wt", "est.rr.wt", "std.rr.wt",
+    "est.or.wt", "std.or.wt"
+  )
+  .wt_estimator
+}
+
+extractStdDiff <- function(.psw) {
+  .df <- data.frame(
+    Vars = row.names(.psw$std.diff.before),
+    Before = unname(.psw$std.diff.before[, 5]),
+    After = unname(.psw$std.diff.after[, 5])
+  )
+  .df
+}
+
+# https://github.com/cran/PSW/blob/master/R/psw-internal.R
+diff.plot <- function(diff.before, diff.after, name, weight, title = NULL) {
+  # plot standardize difference for before and after weighting
+  #
+  # Args:
+  #   diff.before: standardized mean differece before weighting
+  #   diff.after: standardized mean difference after weighting
+  #   name: covariate name
+  #   weight: propensity score weighting method
+  # Returns
+  #   A plot is generated
+
+  par(las = 1, lwd = 2, mar = c(5, max(nchar(name)), 4, 2), bty = "n")
+
+  x.range <- range(c(diff.before, diff.after))
+  y.range <- c(1, length(name))
+  ord <- order(diff.before, decreasing = T)
+  plot(
+    x = x.range, y = y.range, xaxt = "n", yaxt = "n", type = "n",
+    xlim = x.range, ylim = y.range,
+    ylab = "", xlab = "Standardized mean difference",
+    main = title
+  )
+  axis(side = 1, at = pretty(x.range))
+  axis(side = 2, at = length(name):1, labels = name[ord], tick = F)
+  abline(v = 0, lty = 1, col = "gray")
+  abline(v = -10, lty = 3, lwd = 1)
+  abline(v = 10, lty = 3, lwd = 1)
+  points(y = length(name):1, x = diff.before[ord], pch = 4)
+  points(y = length(name):1, x = diff.after[ord], pch = 21)
+  legend("topleft", legend = c("Unadjusted", weight), pch = c(4, 21))
+}
+
+## MI estimates of treatment
+# pool MI results
+# https://stats.idre.ucla.edu/wp-content/uploads/2016/02/multipleimputation.pdf
+PoolEstimates <- function(Q, U) {
+  # Q is point estimate
+  # U is variance estimate
+  if (length(Q) != length(U)) {
+    stop("Q and U are of different length")
+  }
+  m <- length(Q)
+  Q_bar <- mean(Q)
+  U_bar <- mean(U) # within-imputation variance
+  B <- var(Q) # between-imputation variance
+  tt <- U_bar + (1 + 1 / m) * B # total variance
+  .tmp <- c(Q_bar, tt)
+  names(.tmp) <- c("Q_bar", "total_variance")
+  return(.tmp)
+}
