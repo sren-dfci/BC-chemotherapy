7972fd9e9f9ab49a495dc96b6ddf7106f92c6306
sren-dfci <sren@ds.dfci.harvard.edu>
2021-08-02 11:38:54 -0400

    finished it

--- overview.R
+++ overview.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-source("/Users/siyangren/Dropbox (Partners HealthCare)/code repository/Functions/R-functions/CreateSummaryTable.R")
+source("C:/Users/sren/Dropbox (Partners HealthCare)/code repository/Functions/R-functions/CreateSummaryTable.R")
 
 d_path <- file.path(
-  "/Users/siyangren/Dropbox (Partners HealthCare)/BOC shared/Chemo during pregnancy (Sella)/data"
+  "C:/Users/sren/Dropbox (Partners HealthCare)/BOC shared/Chemo during pregnancy (Sella)/data"
 )
 setwd(d_path)
 
@@ -16,26 +16,55 @@ tbl_1_factor_vars <- c(
   "chemo", "gcsf", "surgpg"
 )
 df <- df %>%
-  mutate(across(tbl_1_factor_vars, as.factor))
+  mutate(across(all_of(tbl_1_factor_vars), as.factor))
 # table 2 factor vars
 tbl_2_factor_vars <- c(
   "pregnancy_outcome", "preterm_birth_outcome", "preterm_birth_reason"
 )
 df <- df %>%
-  mutate(across(tbl_2_factor_vars, as.factor))
+  mutate(across(all_of(tbl_2_factor_vars), as.factor))
 # table 3 factor vars
 tbl_3_factor_vars <- c(
   "sptb", "pprom", "gdm", "pih", "chorio",
   "sga", "apgar5v7", "anomalies"
 )
 df <- df %>%
-  mutate(across(tbl_3_factor_vars, as.factor))
+  mutate(across(all_of(tbl_3_factor_vars), as.factor))
 
 
-# --- Generate tables --- # 
-df_chemo <- df %>% filter(chemopg == 1)
-df_no_chemo <- df %>% filter(chemopg == 0)
-df_taxane <- df %>% filter(taxolpreg == 1)
-df_no_taxane <- df %>% filter(taxolpreg == 0)
+# --- Generate tables --- #
+
+# Table 1
+tbl1_vars <- c(
+  "agedx", "gadx", "year_cat", "dxtri", "stage", "cancer_type",
+  "mutation", "chemoga", "chemo_timing", "chemo", "gcsf", "surgpg"
+)
+summaryTable(df, tbl1_vars, "chemopg", .digits = 1)
+summaryTable(df, tbl1_vars, "taxolpreg", .digits = 1)
 
 
+# Table 2
+tbl2_vars <- c(
+  "delga", "pregnancy_outcome",
+  "preterm_birth_outcome", "preterm_birth_reason"
+)
+summaryTable(df, tbl2_vars, "chemopg", .digits = 1)
+
+# Table 3
+# all twin+ pregnancies should be excluded from table 3, 4
+tbl3_vars <- c(
+  "obstetrical_sens", "sptb", "pprom", "gdm", "pih", "chorio",
+  "gestational_sens", "sga", "apgar5v7", "anomalies"
+)
+
+# single birth + chemopg == 1
+df_sub <- df %>% filter(chemopg == 1 & pregnancy_outcome == "single")
+# table 3, split by whether they got taxol during pregnancy
+summaryTable(df_sub, tbl3_vars, "taxolpreg", .digits = 1)
+
+# Table 4
+tbl4_vars <- c(
+  "obstetrical_sens", "sptb", "pprom", "gdm", "pih", "chorio",
+  "gestational_sens", "sga", "apgar5v7", "anomalies"
+)
+summaryTable(df_sub, tbl4_vars, "gfpreg", .digits = 1)
